b1d54b4a0e281dfa69a17d794f954640fdc6636b
Makoto Henmi <mako.mail.g@gmail.com>
2018-04-12 20:44:44 +0900

    base layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import avatar from './avatar.bmp';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div className="container">
+            <a class="navbar-brand" href="#">mako labo</a>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <div className="container-fluid bg-info p-5">
+            <img src={avatar} alt="mako_wismのアイコン" className="rounded-circle mx-auto d-block"/>
+          </div>
+
+        </main>
       </div>
     );
   }
