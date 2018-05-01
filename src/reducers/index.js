af9ea32223d1b5dda35da39a809cd6e0a0058267
Tomohiko Himura <eiel.hal@gmail.com>
2018-05-01 20:38:19 +0900

    機能追加: /moriC にアクセスするともりしーのポートフォリオに

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,15 @@ import type { Person } from '../models/Person';
 
 export type State = { person: Person };
 
-const reducer = (state: State = { person }) => state;
+type Action = { type: 'CHANGE_PERSON', person: Person };
+
+const reducer = (state: State = { person }, action: Action) => {
+  switch (action.type) {
+    case 'CHANGE_PERSON':
+      return { ...state, person: action.person };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,8 +1,14 @@
 /* eslint-env jest */
 // @flow
 import reducer from './index';
+import person, { moriC } from '../models/Person';
 
 it('root reducer', () => {
-  const state = reducer();
+  const state = reducer({ person }, { type: 'CHANGE_PERSON', person });
+  expect(state).toMatchSnapshot();
+});
+
+it('root reducer change other person', () => {
+  const state = reducer({ person }, { type: 'CHANGE_PERSON', person: moriC });
   expect(state).toMatchSnapshot();
 });
