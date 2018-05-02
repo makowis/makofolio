e5180a45bdaca55cec7e4096e6dc7c85d3cad96a
Makoto Henmi <mako.mail.g@gmail.com>
2018-05-03 00:21:26 +0900

    スライドの表示調整

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import type { Slide } from '../models/Slide';
+import { slide as className } from './Slides.css';
 
 type Props = {
   slides: Slide[],
@@ -10,11 +11,15 @@ const Slides = ({ slides }: Props) => (
   <section className="text-center text-white bg-warning p-5">
     <h2>Slides</h2>
     {slides.length > 0 && (
-      <div className="row">
+      <div className="row my-3">
         {slides.map((slide) => (
-          <div className="col-3">
-            <span dangerouslySetInnerHTML={{ __html: slide.content.content }} />
-          </div>
+          <a href={slide.link.href} className="col mb-2">
+            <span
+              dangerouslySetInnerHTML={{ __html: slide.content.content }}
+              className={className}
+            />
+            <p className="text-white my-1">{slide.title}</p>
+          </a>
         ))}
       </div>
     )}
