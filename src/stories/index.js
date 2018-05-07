b1781faa233967feb8f09bcd55dbcb89e35613e4
Tomohiko Himura <eiel.hal@gmail.com>
2018-05-07 21:53:09 +0900

    リファクタリング: storybook backgroundで背景をつけるようにした

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import backgrounds from '@storybook/addon-backgrounds';
 import { withNotes } from '@storybook/addon-notes';
 import fontawesome from '@fortawesome/fontawesome';
 import brands from '@fortawesome/fontawesome-free-brands';
@@ -9,11 +10,13 @@ import FacebookLinkButton from '../components/SnsLinkButton/FacebookLinkButton';
 
 fontawesome.library.add(brands);
 
-storiesOf('SNSLinkButton', module).add(
-  'FacebookLinkButton',
-  withNotes('クリックで指定ユーザのFacebookプロフィールへ遷移します。')(() => (
-    <div style={{ backgroundColor: '#666', padding: '2em' }}>
-      <FacebookLinkButton facebookId="makoto.henmi" />
-    </div>
-  )),
-);
+storiesOf('SNSLinkButton', module)
+  .addDecorator(
+    backgrounds([{ name: 'bg-info', value: '#17a2b8', default: true }]),
+  )
+  .add(
+    'FacebookLinkButton',
+    withNotes('クリックで指定ユーザのFacebookプロフィールへ遷移します。')(
+      () => <FacebookLinkButton facebookId="makoto.henmi" />,
+    ),
+  );
