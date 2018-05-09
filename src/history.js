e94cb3348ed19220d24c4aa9f01af7c87906dbe7
Tomohiko Himura <eiel.hal@gmail.com>
2018-05-09 23:16:21 +0900

    バグ修正: historyがcreateHistory関数でhistoryが生成されていなかった

--- src/history.js
+++ src/history.js
@@ -7,13 +7,14 @@ import type { Store } from 'redux';
 import makowis, { persons } from './models/Person';
 import { changePerson } from './reducers/person';
 
-const createHistory = () => {
+const getCreateHistory = () => {
   // テスト環境ではMemoryHistoryを利用する
   if (process.env.NODE_ENV === 'test') {
     return require('history').createMemoryHistory;
   }
   return require('history').createBrowserHistory;
 };
+const createHistory = getCreateHistory();
 const history = createHistory();
 
 export const setupHistoryHandler = (store: Store) => {
