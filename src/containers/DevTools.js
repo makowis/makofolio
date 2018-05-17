c1d17f2323cd141f0d7e7b5b6ff80c92e126fb2a
Tomohiko Himura <eiel.hal@gmail.com>
2018-05-17 20:52:04 +0900

    バグ修正: 本番環境で出ているエラーを修正
    DevToolsを読み込んでないのに利用しようとするため、表示されてしまう。

--- src/containers/DevTools.js
+++ src/containers/DevTools.js
@@ -1,6 +1,5 @@
 // @flow
 import React from 'react';
-
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { createDevTools } from 'redux-devtools';
 // eslint-disable-next-line import/no-extraneous-dependencies
@@ -8,10 +7,17 @@ import LogMonitor from 'redux-devtools-log-monitor';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import DockMonitor from 'redux-devtools-dock-monitor';
 
-const DevTools = createDevTools(
-  <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
-    <LogMonitor theme="tomorrow" />
-  </DockMonitor>,
-);
+const inDevelopment = process.env.NODE_ENV === 'development';
+
+const buildDevTools = () =>
+  createDevTools(
+    <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-q">
+      <LogMonitor theme="tomorrow" />
+    </DockMonitor>,
+  );
+
+const DevTools = inDevelopment
+  ? buildDevTools()
+  : () => <div style={{ display: 'none' }} />;
 
 export default DevTools;
