bb22c5cae74b5bc2bcd57871d9c635720b824810
Makoto Henmi <mako.mail.g@gmail.com>
2018-05-18 22:20:33 +0900

    lint error fix

--- src/twitter/userURL.js
+++ src/twitter/userURL.js
@@ -2,7 +2,6 @@
 import { toString } from './models/TwitterID';
 import type { TwitterID } from './models/TwitterID';
 
-export const userURL = (id: TwitterID) =>
-  `https://twitter.com/${toString(id)}`;
+export const userURL = (id: TwitterID) => `https://twitter.com/${toString(id)}`;
 
 export default userURL;
